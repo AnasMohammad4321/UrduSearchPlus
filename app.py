acd5b134e6a7ef6f5d00a89afea4866b1e5c8df8
Mohammad Anas <mohammadanas702@gmail.com>
2023-11-19 19:15:02 +0500

    backend-refactoring

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import os
 
 app = Flask(__name__)
 
-# Load doc_id.json, term_id.json, and inverted_index.json outside of the functions
 with open('doc_id.json', 'r', encoding='utf-8') as doc_id_file:
     doc_id_data = json.load(doc_id_file)
 
@@ -16,12 +15,20 @@ with open('term_id.json', 'r', encoding='utf-8') as term_id_file:
 with open('inverted_index.json', 'r', encoding='utf-8') as inverted_index_file:
     inverted_index_data = json.load(inverted_index_file)
 
-# Assign the loaded data to global variables
 doc_id = doc_id_data
 term_id = term_id_data
 inverted_index = inverted_index_data
 
 def preprocess_query(query):
+    """
+    Preprocesses the query by tokenizing and removing stopwords.
+
+    Args:
+        query (str): The input query.
+
+    Returns:
+        list: List of filtered tokens.
+    """
     tokens = word_tokenize(query)
     with open('urdu_stopwords.txt', 'r', encoding='utf-8') as stopwords_file:
         stop_words = stopwords_file.read().splitlines()
@@ -29,12 +36,35 @@ def preprocess_query(query):
     return filtered_tokens
 
 def calculate_tfidf_score(tf, df, total_documents, total_terms_in_doc):
+    """
+    Calculates the TF-IDF score for a term.
+
+    Args:
+        tf (int): Term frequency.
+        df (int): Document frequency.
+        total_documents (int): Total number of documents.
+        total_terms_in_doc (int): Total terms in the document.
+
+    Returns:
+        float: TF-IDF score.
+    """
     idf = math.log10(total_documents / df) if df > 0 else 0
     normalized_tf = tf / total_terms_in_doc if total_terms_in_doc > 0 else 0
     tfidf_score = normalized_tf * idf
     return tfidf_score
 
 def calculate_tfidf_scores(query_term_ids, inverted_index, total_documents):
+    """
+    Calculates TF-IDF scores for the given query term IDs.
+
+    Args:
+        query_term_ids (list): List of query term IDs.
+        inverted_index (dict): Inverted index data structure.
+        total_documents (int): Total number of documents.
+
+    Returns:
+        dict: Dictionary of document IDs and corresponding TF-IDF scores.
+    """
     tfidf_scores = {}
 
     for term_id in query_term_ids:
@@ -52,6 +82,16 @@ def calculate_tfidf_scores(query_term_ids, inverted_index, total_documents):
     return tfidf_scores
 
 def open_and_print_top_documents(sorted_documents, doc_id_mapping):
+    """
+    Retrieves and formats the top documents based on TF-IDF scores.
+
+    Args:
+        sorted_documents (list): List of tuples containing document IDs and TF-IDF scores.
+        doc_id_mapping (dict): Mapping of document IDs to document names.
+
+    Returns:
+        list: List of dictionaries representing top search results.
+    """
     results = []
 
     documents_folder = "Documents"  # Replace with the actual path to your Documents folder
@@ -77,10 +117,22 @@ def open_and_print_top_documents(sorted_documents, doc_id_mapping):
 
 @app.route('/')
 def index():
+    """
+    Renders the index.html template.
+
+    Returns:
+        str: Rendered HTML page.
+    """
     return render_template('index.html')
 
 @app.route('/search', methods=['POST'])
 def search():
+    """
+    Handles the search functionality and returns JSON response.
+
+    Returns:
+        str: JSON response containing search results.
+    """
     if request.method == 'POST':
         query_text = request.form.get('query')
         preprocessed_query = preprocess_query(query_text)
